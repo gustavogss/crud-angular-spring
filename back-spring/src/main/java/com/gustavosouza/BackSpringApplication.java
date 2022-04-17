8675e5270f19042c8f290a9933229ca637537afc
gustavogss <gustavogss.dev@gmail.com>
2022-04-17 03:21:33 -0300

    CourseController criado

--- back-spring/src/main/java/com/gustavosouza/BackSpringApplication.java
+++ back-spring/src/main/java/com/gustavosouza/BackSpringApplication.java
@@ -1,7 +1,12 @@
 package com.gustavosouza;
 
+import com.gustavosouza.model.Course;
+import com.gustavosouza.repository.CourseRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackSpringApplication {
@@ -10,4 +15,15 @@ public class BackSpringApplication {
 		SpringApplication.run(BackSpringApplication.class, args);
 	}
 
+	// Inicializa BD
+	@Bean
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> {
+			courseRepository.deleteAll();
+			Course c = new Course();
+			c.setName("Angular com Spring");
+			c.setCategory("Front-end");
+			courseRepository.save(new Course());
+		};
+	}
 }
--- back-spring/src/main/java/com/gustavosouza/controller/CourseController.java
+++ back-spring/src/main/java/com/gustavosouza/controller/CourseController.java
@@ -3,18 +3,24 @@ package com.gustavosouza.controller;
 import java.util.List;
 
 import com.gustavosouza.model.Course;
+import com.gustavosouza.repository.CourseRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
+
+  private final CourseRepository courseRepository;
 
   @GetMapping
   public List<Course> list() {
-    return null;
+    return courseRepository.findAll();
   }
 
 }
